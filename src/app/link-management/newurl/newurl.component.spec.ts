a2a4b93bed5c6dc0d90b88e4fb8b2c13bd2bc747
malavancs <mlvnhari@gmail.com>
2020-12-23 09:13:54 +0530

    Migrated

--- src/app/link-management/newurl/newurl.component.spec.ts
+++ src/app/link-management/newurl/newurl.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NewurlComponent } from './newurl.component';
 
@@ -6,7 +6,7 @@ describe('NewurlComponent', () => {
   let component: NewurlComponent;
   let fixture: ComponentFixture<NewurlComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NewurlComponent ]
     })
--- src/app/shared/components/inputbox/inputbox.component.spec.ts
+++ src/app/shared/components/inputbox/inputbox.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InputboxComponent } from './inputbox.component';
 
@@ -6,7 +6,7 @@ describe('InputboxComponent', () => {
   let component: InputboxComponent;
   let fixture: ComponentFixture<InputboxComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InputboxComponent ]
     })
